05d34ffe1038f01e3b8c1bfee30bf40d787edb2b
Heya Na <naheya1126@gmail.com>
2016-03-17 01:11:22 -0400

    Added Confusion Matrix

--- src/Bigram.java
+++ src/Bigram.java
@@ -5,8 +5,13 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 
@@ -27,6 +32,65 @@ public class Bigram {
 	static Map<String, ArrayList<Double>> pro_en = new HashMap<String, ArrayList<Double>>();
 	static Map<String, ArrayList<Double>> pro_pt = new HashMap<String, ArrayList<Double>>();
 	
+	//confusion Matrix
+		static int[][] confusionMatrix  = new int[6][6];
+		
+		public static void printMatrix() throws IOException{
+			 File file2 =new File("analysis-bigram.txt");		
+			 //if file doesn't exists, then create it
+		     if(!file2.exists()){
+		    	 file2.createNewFile();
+			 }    				
+		    //true = append file
+			FileWriter fileWritter2 = new FileWriter(file2.getName(),true);
+			BufferedWriter bufferWritter2 = new BufferedWriter(fileWritter2);     	
+			bufferWritter2.write("The confusion matrix of languages(in the order of eu ca gl es en pt):");
+			bufferWritter2.newLine();
+			
+			for(int i = 0; i<6; i++){
+				for(int j = 0; j<6; j++){
+					bufferWritter2.write(confusionMatrix[i][j] + "\t");
+					System.out.print(confusionMatrix[i][j] + "\t");
+				}
+				bufferWritter2.newLine();
+				System.out.println();
+			}
+			bufferWritter2.close();
+		}
+		
+		public static void matrix(String s1, String s2){
+			int i = m(s1);
+			 if(s2.equalsIgnoreCase("eu"))
+				 confusionMatrix[i][0]++;
+			 else if(s2.equalsIgnoreCase("ca"))
+				 confusionMatrix[i][1]++;
+			 else if(s2.equalsIgnoreCase("gl"))
+				 confusionMatrix[i][2]++;
+			 else if(s2.equalsIgnoreCase("es"))
+				 confusionMatrix[i][3]++;
+			 else if(s2.equalsIgnoreCase("en"))
+				 confusionMatrix[i][4]++;
+			 else if(s2.equalsIgnoreCase("pt"))
+				 confusionMatrix[i][5]++;
+		}
+		
+		public static int m(String s1){
+			 int i = 0;
+			 if(s1.equalsIgnoreCase("eu"))
+				 i = 0;
+			 else if(s1.equalsIgnoreCase("ca"))
+				 i = 1;
+			 else if(s1.equalsIgnoreCase("gl"))
+				 i = 2;
+			 else if(s1.equalsIgnoreCase("es"))
+				 i = 3;
+			 else if(s1.equalsIgnoreCase("en"))
+				 i = 4;
+			 else if(s1.equalsIgnoreCase("pt"))
+				 i = 5;
+			 return i;
+		}
+		
 	//calculate the total number of characters in the language
 	public static int calculateSum(Map<String, Integer> hm){
 		int sum = 0;		
@@ -42,7 +106,8 @@ public class Bigram {
 	//calculate the probabilities of one character in the language
 	public static void calculateProbability(String str, Map<String, Integer> hm, 
 			Map<String, ArrayList<Double>> pro) throws IOException{		
-		double probability;
+		double unsmoothedprobability;
+		double smoothedprobability;
 		int sum = calculateSum(hm);
 		Iterator<Map.Entry<String, Integer>> i = hm.entrySet().iterator();		
 		File file =new File("bigramLM.txt");		
@@ -59,14 +124,15 @@ public class Bigram {
 	    int index = 1;
 	    while(i.hasNext()){
 	    	 String key = i.next().getKey();	 
+	    	 unsmoothedprobability = (double)hm.get(key) / sum;
+	         smoothedprobability = (double)(hm.get(key)+ 0.1) / (sum + hm.size()*hm.size()*0.1);
 	         ArrayList<Double> ls = new ArrayList<Double>();
-	         probability = (double)hm.get(key) / sum;
-       	   	 ls.add(probability);
-       	   	 ls.add(probability + 0.1);
+	         ls.add(unsmoothedprobability); //unsmoothed
+       	   	 ls.add(smoothedprobability); //smoothed
 	         pro.put(key, ls);
 	         //System.out.println(probability);
 	         if(index <= 50){
-	        	 bufferWritter.write(index + ". " + key + "\t" + ls.get(0) + "\t\t" + ls.get(1));
+	        	 bufferWritter.write(index + "\t" + key + "\t" + ls.get(0) + "\t\t" + ls.get(1));
 		         bufferWritter.newLine();	   
 		         index++;
 	         } 
@@ -90,7 +156,7 @@ public class Bigram {
 	}
 	
 	public static void parse(String part, Map<String, Integer> hm){
-		for(int i = 0; i < part.length()-1; i++){
+		for(int i = 0; i < part.length() - 1; i++){
        	 	char c1 = part.charAt(i);
        	 	char c2 = part.charAt(i + 1);
 	 		 if(Character.isLetter(c1) && Character.isLetter(c2)){
@@ -105,6 +171,42 @@ public class Bigram {
        }
 	}
 	
+	private static HashMap sortByValues(Map<String, Integer> hm) { 
+	       List list = new LinkedList(hm.entrySet());
+	       // Defined Custom Comparator
+	       Collections.sort(list, new Comparator() {
+	            public int compare(Object o1, Object o2) {
+	               return ((Comparable) ((Map.Entry) (o2)).getValue())
+	                  .compareTo(((Map.Entry) (o1)).getValue());
+	            }
+	       });
+	       // using LinkedHashMap to preserve the insertion order
+	       HashMap sortedHashMap = new LinkedHashMap();
+	       for (Iterator it = list.iterator(); it.hasNext();) {
+	              Map.Entry entry = (Map.Entry) it.next();
+	              sortedHashMap.put(entry.getKey(), entry.getValue());
+	       } 
+	       return sortedHashMap;
+	}
+	
+	//calculate one test sentence's possibility in the given language
+		public static double testData(String s, Map<String, ArrayList<Double>> hm){
+			double probability = 0.0;
+			for(int i = 0; i<s.length() - 1; i++){
+				char c1 = s.charAt(i);
+				char c2 = s.charAt(i + 1);				
+				if(Character.isLetter(c1) && Character.isLetter(c2)){
+					String key = "" + c1 + c2;
+					if(hm.containsKey(key) ){
+						ArrayList<Double> ls = hm.get(key);
+						probability += Math.log10(ls.get(0));
+					}
+				}
+			}
+			System.out.println(probability);
+			return probability;
+	}
+		
 	public static void printHashMap(Map<String, Integer> hm){
 		Iterator<Map.Entry<String, Integer>> i = hm.entrySet().iterator(); 
 	     while(i.hasNext()){
@@ -116,9 +218,11 @@ public class Bigram {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new FileReader("simple-training-tweets_clean.txt"));
+		//BufferedReader in = new BufferedReader(new FileReader("training data.txt"));
 	    String line = "";
 	     while ((line = in.readLine()) != null) {
 	         String parts[] = line.split("\t");
+	         parts[3] = parts[3].toLowerCase();
 	         if(parts[2].equalsIgnoreCase("eu"))
 	        	 parse(parts[3], hm_eu);
 	         else if(parts[2].equalsIgnoreCase("ca"))
@@ -133,6 +237,12 @@ public class Bigram {
 	        	 parse(parts[3], hm_pt);
 	     }
 	     //printHashMap(hm_eu);
+	     hm_eu = sortByValues(hm_eu);
+	     hm_ca = sortByValues(hm_ca);
+	     hm_gl = sortByValues(hm_gl);
+	     hm_es = sortByValues(hm_es);
+	     hm_en = sortByValues(hm_en);
+	     hm_pt = sortByValues(hm_pt);
 	     
 	     calculateProbability("Basque",hm_eu, pro_eu);
 	     calculateProbability("Catalan",hm_ca, pro_ca);
@@ -142,7 +252,126 @@ public class Bigram {
 	     calculateProbability("Portuguese",hm_pt, pro_pt);
 	     
 	     in.close();
+	     
+	     File file =new File("results-bigram.txt");		
 
+		   //if file doesn't exists, then create it
+		     if(!file.exists()){
+				file.createNewFile();
+			}
+				
+			//true = append file
+			FileWriter fileWritter = new FileWriter(file.getName(),true);
+			BufferedWriter bufferWritter = new BufferedWriter(fileWritter);     
+		   		
+			BufferedReader readTestFile = new BufferedReader(new FileReader("simple-testing-tweets_clean.txt"));
+			//BufferedReader readTestFile = new BufferedReader(new FileReader("testing data.txt"));
+			int right = 0;
+			int total = 0;
+			int[] rightEachLang = new int[6];
+			int[] totalEachLang = new int[6];
+			for(int i=0; i<6; i++){
+				rightEachLang[i] = 0;
+				totalEachLang[i] = 0;
+			}
+			
+		     while ((line = readTestFile.readLine()) != null) {
+		         String parts[] = line.split("\t");	         
+		         double max = 0.0;
+		         String lang = "";
+		         double temp = 0.0;
+		         
+		         if(parts[2].equalsIgnoreCase("eu"))
+		        	 totalEachLang[0]++;
+		         else if(parts[2].equalsIgnoreCase("ca"))
+		        	 totalEachLang[1]++;
+		         else if(parts[2].equalsIgnoreCase("gl"))
+		        	 totalEachLang[2]++;
+		         else if(parts[2].equalsIgnoreCase("es"))
+		        	 totalEachLang[3]++;
+		         else if(parts[2].equalsIgnoreCase("en"))
+		        	 totalEachLang[4]++;
+		         else if(parts[2].equalsIgnoreCase("pt"))
+		        	 totalEachLang[5]++;
+		         
+		         max = testData(parts[3], pro_eu);	         
+		         lang = "eu";
+		         
+		         temp = testData(parts[3], pro_ca);
+		         if(max < temp){
+		        	 max = temp;
+		        	 lang = "ca";
+		         } 
+		         
+		         temp = testData(parts[3], pro_gl);
+		         if(max < temp){
+		        	 max = temp;
+		        	 lang = "gl";
+		         }
+		         
+		         temp = testData(parts[3], pro_es);
+		         if(max < temp){
+		        	 max = temp;
+		        	 lang = "es";
+		         } 
+		         
+		         temp = testData(parts[3], pro_en);
+		         if(max < temp){
+		        	 max = temp;
+		        	 lang = "en";
+		         } 
+		         
+		         temp = testData(parts[3], pro_pt);
+		         if(max < temp){
+		        	 max = temp;
+		        	 lang = "pt";
+		         } 
+		         bufferWritter.write(parts[0] + "\t" + parts[2] + "\t" + lang);
+		         bufferWritter.newLine();
+		         total++;
+		         if(lang.equalsIgnoreCase(parts[2])){
+		        	 right++;	
+		        	 if(lang.equalsIgnoreCase("eu"))
+			        	 rightEachLang[0]++;
+			         else if(lang.equalsIgnoreCase("ca"))
+			        	 rightEachLang[1]++;
+			         else if(lang.equalsIgnoreCase("gl"))
+			        	 rightEachLang[2]++;
+			         else if(lang.equalsIgnoreCase("es"))
+			        	 rightEachLang[3]++;
+			         else if(lang.equalsIgnoreCase("en"))
+			        	 rightEachLang[4]++;
+			         else if(lang.equalsIgnoreCase("pt"))
+			        	 rightEachLang[5]++;
+		         }
+		         matrix(parts[2], lang);
+		     }
+		     double result = (double)right / total;
+		     System.out.println(result);
+		     
+		     File file2 =new File("analysis-bigram.txt");		
+			 //if file doesn't exists, then create it
+		     if(!file2.exists()){
+		    	 file2.createNewFile();
+			 }    				
+		    //true = append file
+			FileWriter fileWritter2 = new FileWriter(file2.getName(),true);
+			BufferedWriter bufferWritter2 = new BufferedWriter(fileWritter2);     	
+			bufferWritter2.write("The accuracy of each language(in the order of eu ca gl es en pt):");
+			bufferWritter2.newLine();
+			bufferWritter2.write("right\t" + "total\t" + "accuracy");
+			bufferWritter2.newLine();
+		    for(int i = 0; i<6; i++){
+		    	//System.out.println((double)rightEachLang[i]/totalEachLang[i]);
+		    	bufferWritter2.write(rightEachLang[i]+ "\t" + totalEachLang[i] + "\t" +
+		    			Double.toString((double)rightEachLang[i]/totalEachLang[i]));
+		    	bufferWritter2.newLine();
+		    }
+		    
+		     bufferWritter.close();
+		     readTestFile.close();
+		     bufferWritter2.close();
+		     printMatrix();
 	}
 
 }
--- src/Uni.java
+++ src/Uni.java
@@ -3,7 +3,7 @@ import java.util.*;
 import java.util.Map.Entry;
 
 public class Uni {
-	//create 6 hash maps for the 6 languages. Key is the character, value is its frequency
+	//create one hashmap for each language. Key is the character, value is its frequency.
 	static Map<Character, Integer> hm_eu = new HashMap<Character, Integer>();
 	static Map<Character, Integer> hm_ca = new HashMap<Character, Integer>();
 	static Map<Character, Integer> hm_gl = new HashMap<Character, Integer>();
@@ -11,13 +11,73 @@ public class Uni {
 	static Map<Character, Integer> hm_en = new HashMap<Character, Integer>();
 	static Map<Character, Integer> hm_pt = new HashMap<Character, Integer>();
 	
-	//6 hash maps for the probabilities. Key is the character, value is its probabilities
+	//create one hashmap for each language. Key is the character, value is an arrayList which contains its
+	//smoothed and unsmoothed probability
 	static Map<Character, ArrayList<Double>> pro_eu = new HashMap<Character, ArrayList<Double>>();
 	static Map<Character, ArrayList<Double>> pro_ca = new HashMap<Character, ArrayList<Double>>();
 	static Map<Character, ArrayList<Double>> pro_gl = new HashMap<Character, ArrayList<Double>>();
 	static Map<Character, ArrayList<Double>> pro_es = new HashMap<Character, ArrayList<Double>>();
 	static Map<Character, ArrayList<Double>> pro_en = new HashMap<Character, ArrayList<Double>>();
 	static Map<Character, ArrayList<Double>> pro_pt = new HashMap<Character, ArrayList<Double>>();
+	
+	//confusion Matrix
+	static int[][] confusionMatrix  = new int[6][6];
+	
+	public static void printMatrix() throws IOException{
+		 File file2 =new File("analysis-unigram.txt");		
+		 //if file doesn't exists, then create it
+	     if(!file2.exists()){
+	    	 file2.createNewFile();
+		 }    				
+	    //true = append file
+		FileWriter fileWritter2 = new FileWriter(file2.getName(),true);
+		BufferedWriter bufferWritter2 = new BufferedWriter(fileWritter2);     	
+		bufferWritter2.write("The confusion matrix of languages(in the order of eu ca gl es en pt):");
+		bufferWritter2.newLine();
+		
+		for(int i = 0; i<6; i++){
+			for(int j = 0; j<6; j++){
+				bufferWritter2.write(confusionMatrix[i][j] + "\t");
+				System.out.print(confusionMatrix[i][j] + "\t");
+			}
+			bufferWritter2.newLine();
+			System.out.println();
+		}
+		bufferWritter2.close();
+	}
+	
+	public static void matrix(String s1, String s2){
+		int i = m(s1);
+		 if(s2.equalsIgnoreCase("eu"))
+			 confusionMatrix[i][0]++;
+		 else if(s2.equalsIgnoreCase("ca"))
+			 confusionMatrix[i][1]++;
+		 else if(s2.equalsIgnoreCase("gl"))
+			 confusionMatrix[i][2]++;
+		 else if(s2.equalsIgnoreCase("es"))
+			 confusionMatrix[i][3]++;
+		 else if(s2.equalsIgnoreCase("en"))
+			 confusionMatrix[i][4]++;
+		 else if(s2.equalsIgnoreCase("pt"))
+			 confusionMatrix[i][5]++;
+	}
+	
+	public static int m(String s1){
+		 int i = 0;
+		 if(s1.equalsIgnoreCase("eu"))
+			 i = 0;
+		 else if(s1.equalsIgnoreCase("ca"))
+			 i = 1;
+		 else if(s1.equalsIgnoreCase("gl"))
+			 i = 2;
+		 else if(s1.equalsIgnoreCase("es"))
+			 i = 3;
+		 else if(s1.equalsIgnoreCase("en"))
+			 i = 4;
+		 else if(s1.equalsIgnoreCase("pt"))
+			 i = 5;
+		 return i;
+	}
 	
 	//calculate the total number of characters in the language
 	public static int calculateSum(Map<Character, Integer> hm){
@@ -34,16 +94,15 @@ public class Uni {
 	//calculate the probabilities of one character in the language
 	public static void calculateProbability(String str, Map<Character, Integer> hm, 
 			Map<Character, ArrayList<Double>> pro) throws IOException{		
-		double probability;
+		double unsmoothedprobability;
+		double smoothedprobability;
 		int sum = calculateSum(hm);
-		Iterator<Map.Entry<Character, Integer>> i = hm.entrySet().iterator();
-		
+		Iterator<Map.Entry<Character, Integer>> i = hm.entrySet().iterator();		
 		File file =new File("unigramLM.txt");		
 		//if file doesn't exists, then create it
 		if(!file.exists()){
 			file.createNewFile();
-		}
-		
+		}		
 		//true = append file
 		FileWriter fileWritter = new FileWriter(file.getName(),true);
 	    BufferedWriter bufferWritter = new BufferedWriter(fileWritter);     
@@ -52,10 +111,11 @@ public class Uni {
 	    
 	    while(i.hasNext()){
 	         Character key = i.next().getKey();	 
-	         probability = (double)hm.get(key) / sum;
+	         unsmoothedprobability = (double)hm.get(key) / sum;
+	         smoothedprobability = (double)(hm.get(key)+ 0.1) / (sum + hm.size()*0.1);
 	         ArrayList<Double> ls = new ArrayList<Double>();
-       	   	 ls.add(probability);
-       	   	 ls.add(probability + 0.1);
+       	   	 ls.add(unsmoothedprobability); //unsmoothed
+       	   	 ls.add(smoothedprobability); //smoothed
 	         pro.put(key, ls);
 	         //System.out.println(probability);
 	         bufferWritter.write(key + "\t" + ls.get(0) + "\t\t" + ls.get(1));
@@ -72,7 +132,7 @@ public class Uni {
 			if(Character.isLetter(c)){
 				if(hm.containsKey(c)){
 					ArrayList<Double> ls = hm.get(c);
-					probability += Math.log10(ls.get(0));
+					probability += Math.log10(ls.get(0)); 
 				}
 			}
 		}
@@ -95,15 +155,13 @@ public class Uni {
 	
 	private static HashMap sortByValues(Map<Character, Integer> hm) { 
 	       List list = new LinkedList(hm.entrySet());
-	       // Defined Custom Comparator here
+	       // Defined Custom Comparator
 	       Collections.sort(list, new Comparator() {
 	            public int compare(Object o1, Object o2) {
 	               return ((Comparable) ((Map.Entry) (o2)).getValue())
 	                  .compareTo(((Map.Entry) (o1)).getValue());
 	            }
 	       });
-
-	       // Here I am copying the sorted list in HashMap
 	       // using LinkedHashMap to preserve the insertion order
 	       HashMap sortedHashMap = new LinkedHashMap();
 	       for (Iterator it = list.iterator(); it.hasNext();) {
@@ -114,19 +172,22 @@ public class Uni {
 	}
 	
 	public static void printHashMap(Map<Character, Integer> hm){
+		System.out.println("-------------------Map----------------------");
 		Iterator<Map.Entry<Character, Integer>> i = hm.entrySet().iterator(); 
 	     while(i.hasNext()){
 	         Character key = i.next().getKey();
 	         System.out.println(key+": "+hm.get(key));
 	     }
-	     System.out.println("-------------------------------------------");
+	    System.out.println("-------------------------------------------");
 	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new FileReader("simple-training-tweets_clean.txt"));
+		//BufferedReader in = new BufferedReader(new FileReader("training data.txt"));
 	    String line = "";
 	     while ((line = in.readLine()) != null) {
 	         String parts[] = line.split("\t");
+	         parts[3] = parts[3].toLowerCase();
 	         if(parts[2].equalsIgnoreCase("eu"))
 	        	 parse(parts[3], hm_eu);
 	         else if(parts[2].equalsIgnoreCase("ca"))
@@ -142,25 +203,146 @@ public class Uni {
 	     }
 	      
 	     hm_eu = sortByValues(hm_eu);
-	     printHashMap(hm_eu);
+	     hm_ca = sortByValues(hm_ca);
+	     hm_gl = sortByValues(hm_gl);
+	     hm_es = sortByValues(hm_es);
+	     hm_en = sortByValues(hm_en);
+	     hm_pt = sortByValues(hm_pt);
+//	     printHashMap(hm_eu);
 //	     printHashMap(hm_ca);
 //	     printHashMap(hm_gl);
 //	     printHashMap(hm_es);
 //	     printHashMap(hm_en);
 //	     printHashMap(hm_pt);
 	     calculateProbability("Basque",hm_eu, pro_eu);
-//	     calculateProbability("Catalan",hm_ca, pro_ca);
-//	     calculateProbability("Galician",hm_gl, pro_gl);
-//	     calculateProbability("Spanish",hm_es, pro_es);
-//	     calculateProbability("English",hm_en, pro_en);
-//	     calculateProbability("Portuguese",hm_pt, pro_pt);
+	     calculateProbability("Catalan",hm_ca, pro_ca);
+	     calculateProbability("Galician",hm_gl, pro_gl);
+	     calculateProbability("Spanish",hm_es, pro_es);
+	     calculateProbability("English",hm_en, pro_en);
+	     calculateProbability("Portuguese",hm_pt, pro_pt);
 	     in.close();
 	     
-	     BufferedReader readTestFile = new BufferedReader(new FileReader("simple-testing-tweets_clean.txt"));
-	     while ((line = in.readLine()) != null) {
-	         String parts[] = line.split("\t");
-	        
+	     File file =new File("results-unigram.txt");		
+
+	   //if file doesn't exists, then create it
+	     if(!file.exists()){
+			file.createNewFile();
+		}
+			
+		//true = append file
+		FileWriter fileWritter = new FileWriter(file.getName(),true);
+		BufferedWriter bufferWritter = new BufferedWriter(fileWritter);     
+	   		
+		BufferedReader readTestFile = new BufferedReader(new FileReader("simple-testing-tweets_clean.txt"));
+		//BufferedReader readTestFile = new BufferedReader(new FileReader("testing data.txt"));
+		int right = 0;
+		int[] rightEachLang = new int[6];
+		int[] totalEachLang = new int[6];
+		int total = 0;
+		
+		for(int i=0; i<6; i++){
+			rightEachLang[i] = 0;
+			totalEachLang[i] = 0;
+		}
+		
+	     while ((line = readTestFile.readLine()) != null) {
+	         String parts[] = line.split("\t");	         
+	         double max = 0.0;
+	         String lang = "";
+	         double temp = 0.0;
+	         
+	         if(parts[2].equalsIgnoreCase("eu"))
+	        	 totalEachLang[0]++;
+	         else if(parts[2].equalsIgnoreCase("ca"))
+	        	 totalEachLang[1]++;
+	         else if(parts[2].equalsIgnoreCase("gl"))
+	        	 totalEachLang[2]++;
+	         else if(parts[2].equalsIgnoreCase("es"))
+	        	 totalEachLang[3]++;
+	         else if(parts[2].equalsIgnoreCase("en"))
+	        	 totalEachLang[4]++;
+	         else if(parts[2].equalsIgnoreCase("pt"))
+	        	 totalEachLang[5]++;
+	         
+	         max = testData(parts[3], pro_eu);
+	         lang = "eu";
+	         
+	         temp = testData(parts[3], pro_ca);
+	         if(max < temp){
+	        	 max = temp;
+	        	 lang = "ca";
+	         } 
+	         
+	         temp = testData(parts[3], pro_gl);
+	         if(max < temp){
+	        	 max = temp;
+	        	 lang = "gl";
+	         }
+	         
+	         temp = testData(parts[3], pro_es);
+	         if(max < temp){
+	        	 max = temp;
+	        	 lang = "es";
+	         } 
+	         
+	         temp = testData(parts[3], pro_en);
+	         if(max < temp){
+	        	 max = temp;
+	        	 lang = "en";
+	         } 
+	         
+	         temp = testData(parts[3], pro_pt);
+	         if(max < temp){
+	        	 max = temp;
+	        	 lang = "pt";
+	         } 
+	         
+	         bufferWritter.write(parts[0] + "\t" + parts[2] + "\t" + lang);
+	         bufferWritter.newLine();
+	         total++;
+	         if(lang.equalsIgnoreCase(parts[2])){
+	        	 right++;	
+	        	 if(lang.equalsIgnoreCase("eu"))
+		        	 rightEachLang[0]++;
+		         else if(lang.equalsIgnoreCase("ca"))
+		        	 rightEachLang[1]++;
+		         else if(lang.equalsIgnoreCase("gl"))
+		        	 rightEachLang[2]++;
+		         else if(lang.equalsIgnoreCase("es"))
+		        	 rightEachLang[3]++;
+		         else if(lang.equalsIgnoreCase("en"))
+		        	 rightEachLang[4]++;
+		         else if(lang.equalsIgnoreCase("pt"))
+		        	 rightEachLang[5]++;
+	         }
+	         matrix(parts[2], lang);
 	     }
+	     double result = (double)right / total;
+	     //System.out.println(result);
+	     
+	     File file2 =new File("analysis-unigram.txt");		
+		 //if file doesn't exists, then create it
+	     if(!file2.exists()){
+	    	 file2.createNewFile();
+		 }    				
+	    //true = append file
+		FileWriter fileWritter2 = new FileWriter(file2.getName(),true);
+		BufferedWriter bufferWritter2 = new BufferedWriter(fileWritter2);     	
+		bufferWritter2.write("The accuracy of each language(in the order of eu ca gl es en pt):");
+		bufferWritter2.newLine();
+		bufferWritter2.write("right\t" + "total\t" + "accuracy");
+		bufferWritter2.newLine();
+	    for(int i = 0; i<6; i++){
+	    	//System.out.println((double)rightEachLang[i]/totalEachLang[i]);
+	    	bufferWritter2.write(rightEachLang[i]+ "\t" + totalEachLang[i] + "\t" +
+	    			Double.toString((double)rightEachLang[i]/totalEachLang[i]));
+	    	bufferWritter2.newLine();
+	    }
+	     
+	    bufferWritter.close();
+	    readTestFile.close();
+	    bufferWritter2.close();
+	    printMatrix();
 	}
 
 }
